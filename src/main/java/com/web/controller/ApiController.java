7f159f6ff931d92ba51d189b60b725bbbccf8db9
cheolll <wkdcjf9204@gmail.com>
2024-02-14 15:04:43 +0900

    TEST

---
+++ src/main/java/com/web/controller/ApiController.java
@@ -0,0 +1,9 @@
+package com.web.controller;
+
+public class ApiController {
+	
+	
+	String key = "wGgAMNctzAVjo7O4ZlwZPcCNHPr9t8IPlm4lYhfG1RbY79FR2pL%2BnAhWAyP0%2FObPwgvONXIi1Ke1UTRujCO%2Fnw%3D%3D";
+	
+
+}
